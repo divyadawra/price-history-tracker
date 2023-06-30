4d0d94b1f64acb0b175027284819317fc46419f5
divyadawra <65996223+divyadawra@users.noreply.github.com>
2023-06-30 19:33:56 +0530

    Update amazon.module.ts

--- src/sources/amazon/amazon.module.ts
+++ src/sources/amazon/amazon.module.ts
@@ -3,9 +3,10 @@ import { AmazonController } from './amazon.controller';
 import { AmazonService } from './amazon.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Amazon } from './amazon.entity';
+import { ProductModule } from 'src/product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Amazon])],
+  imports: [TypeOrmModule.forFeature([Amazon]), ProductModule],
   controllers: [AmazonController],
   providers: [AmazonService],
 })
