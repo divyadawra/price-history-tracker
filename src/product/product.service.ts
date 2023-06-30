22e81c52e4858823b1d533dd0407a3aaa6a56036
divyadawra <65996223+divyadawra@users.noreply.github.com>
2023-06-30 19:36:14 +0530

    Update product.service.ts

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -8,13 +8,19 @@ export class ProductService {
 
     constructor(
         @InjectRepository(Product)
-        private productsRepository: Repository<Product>, 
-        private dataSource: DataSource
+        private productsRepository: Repository<Product>
       ) {}
+
+  getProductByName(name:string): Promise<Product | null> {
+    return this.productsRepository.findOneBy({ name });
+  }
+
+  getAllProducts(): Promise<Product[]> {
+    return this.productsRepository.find();
+  }
   getProduct(id:number): Promise<Product | null> {
     return this.productsRepository.findOneBy({ id });
   }
-
   saveProduct(product:Product): Promise<Product> {
     return this.productsRepository.save(product);
   }
