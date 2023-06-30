60e59127e15a9df3455e9f425cd6b17ec0332760
divyadawra <65996223+divyadawra@users.noreply.github.com>
2023-06-30 19:38:53 +0530

    Update flipkart.module.ts

--- src/sources/flipkart/flipkart.module.ts
+++ src/sources/flipkart/flipkart.module.ts
@@ -3,9 +3,10 @@ import { FlipkartController } from './flipkart.controller';
 import { FlipkartService } from './flipkart.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Flipkart } from './flipkart.entity';
+import { ProductModule } from 'src/product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Flipkart])],
+  imports: [TypeOrmModule.forFeature([Flipkart]), ProductModule],
   controllers: [FlipkartController],
   providers: [FlipkartService],
 })
